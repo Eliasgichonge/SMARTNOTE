a08136b48256e7f966423e51df41106a694d6b2e
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-02-17 02:38:02 +0300

    Displaying content of a singe note

--- notes/views.py
+++ notes/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import Http404
 
+from notes.models import Note
+
+
 def list(request):
     all_notes = Note.objects.all()
     return render(request, 'notes/notes_list.html', {'notes': all_notes})
 
 def detail(request, pk):
-    note = Note.objects.get(pk=pk)
+    try:
+        note = Note.objects.get(pk=pk)
+    except Note.DoesNotExist:
+        raise Http404("Note doesn't exist")
     return render(request, 'notes/notes_detail.html', {'note': note})
