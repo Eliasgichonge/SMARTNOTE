1ff31b7d95cb5d640abf435690548c5db7a30801
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-02-17 02:41:29 +0300

    Introduction to Django class based-views

--- home/views.py
+++ home/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from  datetime import datetime
 from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+
 
 def home(request):
     return render(request, 'home/welcome.html', {'today': datetime.today()})
