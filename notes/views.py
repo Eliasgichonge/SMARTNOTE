c65b2d21a156887b2feca3a2ccba111901d8345a
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-02-17 02:25:15 +0300

    Displaying content of a singe note

--- notes/views.py
+++ notes/views.py
@@ -4,3 +4,7 @@ from  . models import Note
 def list(request):
     all_notes = Note.objects.all()
     return render(request, 'notes/notes_list.html', {'notes': all_notes})
+
+def detail(request, pk):
+    note = Note.objects.get(pk=pk)
+    return render(request, 'notes/note_detail.html', {'note': note})
