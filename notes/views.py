705dc9c79c486557ca3243fcd278a6ed8bcccede
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-02-17 06:50:07 +0300

    A bit more on class based-view

--- notes/views.py
+++ notes/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 from django.http import Http404
 from django.views.generic import ListView
 
+
+class NotesListView(ListView):
+      model = Notes
+      context_objects_name = 'notes'
+
 def list(request):
     all_notes = Notes.objects.all()
     return render(request, 'notes/notes_list.html', {'notes': all_notes})
