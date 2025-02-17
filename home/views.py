aef02c141e41b2f56783d10eb21970293e246279
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-02-17 06:39:51 +0300

    Introduction to Django class base-view

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
-from  django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
 
