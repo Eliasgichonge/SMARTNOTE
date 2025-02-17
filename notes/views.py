70593fdebec2f9e2481f689b849e1a7c62d32b16
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-02-17 21:59:02 +0300

    Creating Dynamic template

--- notes/views.py
+++ notes/views.py
@@ -4,3 +4,8 @@ from . models import Notes
 def list(request):
     all_notes = Notes.objects.all()
     return render(request, 'notes/notes_list.html', {'notes': all_notes})
+
+
+def detail(request, id):
+    note = Notes.objects.get(id=id)
+    return render(request, 'notes/notes_detail.html', {'note': note})
