ec7d4a7e5915718e05a5697bf81c7c5a4b1d126b
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-02-17 04:30:05 +0300

    User authentication int two simple steps

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
-from  django.contib.auth.decorators import login_required
+from  django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, 'home/welcome.html', {'today': datetime.today()})
