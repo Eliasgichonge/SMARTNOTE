44fb43d4adea9e8a5b845448ef99893ba0debfa2
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-02-17 04:29:24 +0300

    User authentication int two simple steps

--- home/views.py
+++ home/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from  django.contib.auth.decorators import login_required
 
 def home(request):
     return render(request, 'home/welcome.html', {'today': datetime.today()})
 
 
+@login_required(login_url='/admin/')
 def authorized(request):
-    return render(request, 'home/authorized.html', {'title': 'About'})
+    return render(request, 'home/authorized.html', {})
