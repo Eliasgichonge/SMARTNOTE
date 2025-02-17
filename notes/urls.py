e14b03d20b0f7b126e126772df63e0b0ba8fadbc
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-02-17 03:25:42 +0300

    A bit more on class base-views

--- notes/urls.py
+++ notes/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes/', views.NoteListView.as_view(), name='note-list'),
+    path('notes/', views.NoteListView.as_view()),
     path('notes/<int:pk>/', views.detail, name='note-detail'),
 ]
