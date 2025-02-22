3943dadff3347f623d9fee35331db897aaccb79e
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-02-22 12:15:32 +0300

    Using admin for data creation and manipulation

--- notes/admin.py
+++ notes/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from . import models
 
 class NotesAdmin(admin.ModelAdmin):
-      list_display = ('title')
+      list_display = ('title',)
 
 admin.site.register(models.Note, NotesAdmin)
