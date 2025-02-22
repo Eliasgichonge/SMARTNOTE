040cd2066843627b7b838a7b6265e1d8d42f2f45
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-02-22 11:45:14 +0300

    User authentication in 2 simple steps

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
