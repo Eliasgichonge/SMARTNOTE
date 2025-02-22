6194d8c8eeef70ac1c53a46ad6603484cafd943e
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-02-22 14:05:28 +0300

    Server collapse

--- notes/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Note
-
-def list(request):  # Renamed function from "list" to "notes_list"
-    all_notes = Note.objects.all()  # Get all notes from the database
-    return render(request, 'notes/notes_list.html', {'notes': all_notes})
