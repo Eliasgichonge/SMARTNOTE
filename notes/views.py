c72148a3e696e618a4f99824a8f8a6ad19c15b78
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-02-22 13:32:57 +0300

    Creating dynamic template

--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Note
 
-def notes_list(request):  # Renamed function from "list" to "notes_list"
+def list(request):  # Renamed function from "list" to "notes_list"
     all_notes = Note.objects.all()  # Get all notes from the database
     return render(request, 'notes/notes_list.html', {'notes': all_notes})
