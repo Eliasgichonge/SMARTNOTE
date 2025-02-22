4a8842336d3da9b2c5338e39907bdbd0ea057e45
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-02-22 11:59:18 +0300

    Introduction to Django ORMs

--- home/views.py
+++ home/views.py
@@ -4,7 +4,6 @@ from datetime import datetime
 from django.contrib.auth.decorators import login_required
 
 
-
 def home(request):
     return render(request, 'home/welcome.html', {'today': datetime.today()})
 
