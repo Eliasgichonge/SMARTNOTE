9a7d506285d7ca5c1e150d6b38690a0abe0218c4
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-02-28 11:36:50 +0300

    User authentication in 2 simple steps

--- home/views.py
+++ home/views.py
@@ -8,7 +8,7 @@ def home(request):
     return render(request, 'home/welcome.html', {'today': datetime.today()})
 
 
-@login_required
+@login_required(login_url='/admin')
 def authorized(request):
     return render(request, 'home/authorized.html', {})
 
