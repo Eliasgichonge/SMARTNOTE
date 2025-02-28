57d43f7cf9da35c1cb6df83d945720522031af23
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-02-28 11:05:56 +0300

    Creating first Django Template

--- home/views.py
+++ home/views.py
@@ -4,6 +4,6 @@ from datetime import datetime
 
 
 def home(request):
-    return render(request, 'home/welcome.html', {'today': datetime.today})
+    return render(request, 'home/welcome.html', {'today': datetime.today()})
 
 
