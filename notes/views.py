1b252a98e75c56641965586fc901f4c5a906ee60
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-02-28 13:39:44 +0300

    A bit more on class-based views

--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import Http404
 from .models import Notes
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 
 class NotesListView(ListView):
