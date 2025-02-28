08834a87646b1a48504498cf67464ea865e5147f
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-02-28 11:46:25 +0300

    Creating models

--- notes/models.py
+++ notes/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Notes(models.Model):
       title = models.CharField(max_length=200)
       text = models.TextField()
-      created = models.DateTimeField()
+      created = models.DateTimeField(auto_now_add=True)
