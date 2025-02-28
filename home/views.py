c90328e6d502032c77ffdafc310608e0bc9737a0
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-02-28 13:26:31 +0300

    Introduction to Django class-based views

--- home/views.py
+++ home/views.py
@@ -14,6 +14,7 @@ class HomeView(TemplateView):
 
 class AuthorizedView(LoginRequiredMixin, TemplateView):
       template_name = 'home/authorized.html'
+      login_url = '/admin'
 
 @login_required(login_url='/admin')
 def authorized(request):
