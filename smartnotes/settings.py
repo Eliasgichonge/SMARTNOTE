4daef093d98696e5b0a55ec10b181b87bfa2932a
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-02-28 14:10:59 +0300

    How to set up a base HTML for every Django template

--- smartnotes/settings.py
+++ smartnotes/settings.py
@@ -59,7 +59,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-               BASE_DIR / 'static/templates'
+               BASE_DIR / 'static/templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
