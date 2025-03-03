ca632421008b585a114333422258bd7cefdd75a7
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-03-03 21:24:13 +0300

    Add some Styles

--- notes/urls.py
+++ notes/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.NotesListView.as_view(), name=""),
+    path('', views.NotesListView.as_view(), name="notes.list"),
     path('notes/<int:pk>', views.NotesDetailView.as_view()),
 ]
