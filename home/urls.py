4ba6152d87c47cc35d1a694f271d757c04ba8c38
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-03-05 14:15:15 +0300

    Adding login logout pages

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.HomeView.as_view()),
+    path('home', views.HomeView.as_view(), name='home'),
     path('login', views.LoginInterfaceView.as_view()),
     path('logout', views.LogoutInterfaceView.as_view()),
 ]
