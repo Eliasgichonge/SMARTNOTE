c7e6fadd7a3f1a6f9d272eb2d0bb2985a8a1b75c
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-03-05 13:44:10 +0300

    Adding login logout pages

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('home', views.HomeView.as_view()),
-    path('authorized', views.AuthorizedView.as_view())
+    path('authorized', views.AuthorizedView.as_view()),
 ]
