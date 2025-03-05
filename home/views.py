ceab2f3e888b8ea4c0acfcf86b4af4289b6d0a7c
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-03-05 13:24:13 +0300

    Adding login logout pages

--- home/views.py
+++ home/views.py
@@ -8,6 +8,10 @@ from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView
 
 
+class LoginInterfaceView(LoginView):
+      template_name = 'home/login.html'
+
+
 class HomeView(TemplateView):
       template_name = 'home/welcome.html'
       extra_context = {'today': datetime.today()}
