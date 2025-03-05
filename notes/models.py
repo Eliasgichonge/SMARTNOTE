db7b103c24241f4d3e648da3a9c5eb623a7d57b8
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-03-05 12:42:52 +0300

    ForeignKey used to relate each other

--- notes/models.py
+++ notes/models.py
@@ -6,3 +6,4 @@ class Notes(models.Model):
       title = models.CharField(max_length=200)
       text = models.TextField()
       created = models.DateTimeField(auto_now_add=True)
+      user = models.ForeignKey()
